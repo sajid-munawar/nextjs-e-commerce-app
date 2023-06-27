5b4c08d68f22b390f6ec345654591add4ef3240e
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-27 08:58:17 +0500

    Removed svgs and used react-icons

--- src/components/Nav/MobilePopUp.tsx
+++ src/components/Nav/MobilePopUp.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import { GrClose } from "react-icons/gr";
+import { RiMenu3Line } from "react-icons/ri";
+import { BsCart } from "react-icons/bs";
 
 export default function MobilePopUp() {
   const [open, setOpen] = useState(false);
@@ -16,44 +19,11 @@ export default function MobilePopUp() {
         <div onClick={handleOpen} className="absolute right-10 top-10 z-20">
           {!open ? (
             <>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 24 24"
-                color="black"
-                fontSize="27"
-                //   style="color:black"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M3 4h18v2H3V4zm6 7h12v2H9v-2zm-6 7h18v2H3v-2z"></path>
-                </g>
-              </svg>
+              <RiMenu3Line size={25} />
             </>
           ) : (
             <>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 24 24"
-                color="black"
-                fontSize="27"
-                //   class="close_icon"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-                //   style="color: black;"
-              >
-                <g>
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"></path>
-                </g>
-              </svg>
+              <GrClose size={25} />
             </>
           )}
         </div>
@@ -75,30 +45,7 @@ export default function MobilePopUp() {
               {/* Cart button */}
               <Link onClick={() => setOpen(!open)} href="/cart">
                 <button className="relative rounded-full bg-gray-200 p-4">
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="0"
-                    viewBox="0 0 24 24"
-                    height="22"
-                    width="22"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
-                      fill="currentColor"
-                    ></path>
-                    <path
-                      d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
-                      fill="currentColor"
-                    ></path>
-                    <path
-                      d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
+                  <BsCart size={25} />
                   <span className="absolute right-1 top-0 h-6 w-6 rounded-full bg-red-600 text-white">
                     0
                   </span>
