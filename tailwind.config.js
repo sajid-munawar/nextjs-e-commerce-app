02f67af0d0b1fb5b5eca33625c9d190fdcc8ed2e
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-27 10:39:59 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        lighBlue: "#e1edff",
+        textSaleBlue: "#0000ff",
+        textBlue: "#0062f5",
+        blackButton: "#212121",
+        textBlack: "#212121",
+        textGrey: "#666",
+        circleCream: "#ffece3",
+        backgroundGrey: "#f2f3f7",
+      },
     },
   },
   plugins: [],
