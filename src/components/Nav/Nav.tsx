367a9b45b027061e0ab2ac32dec9020bb149590a
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-27 09:03:49 +0500

    Removed search svg and used react icon instead

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import MobilePopUp from "./MobilePopUp";
 import Link from "next/link";
 import NavCart from "./NavCart";
+import { CiSearch } from "react-icons/ci";
 
 export default function Nav() {
   return (
@@ -37,28 +38,7 @@ export default function Nav() {
 
           {/* Search bar */}
           <div className="flex h-6 items-center rounded-md border-2 border-gray-300 px-2">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              version="1.1"
-              id="search"
-              x="0px"
-              y="0px"
-              viewBox="0 0 24 24"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path
-                  d="M20.031,20.79c0.46,0.46,1.17-0.25,0.71-0.7l-3.75-3.76c1.27-1.41,2.04-3.27,2.04-5.31
-		c0-4.39-3.57-7.96-7.96-7.96s-7.96,3.57-7.96,7.96c0,4.39,3.57,7.96,7.96,7.96c1.98,0,3.81-0.73,5.21-1.94L20.031,20.79z
-		 M4.11,11.02c0-3.84,3.13-6.96,6.96-6.96c3.84,0,6.96,3.12,6.96,6.96c0,3.84-3.12,6.96-6.96,6.96C7.24,17.98,4.11,14.86,4.11,11.02
-		z"
-                ></path>
-              </g>
-            </svg>
+            <CiSearch />
             <input
               className=" max-w-sm rounded-md bg-white px-2 text-sm"
               type="search"
