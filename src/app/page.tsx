7075a8c7495067abddbf8a8656abda2ba621bf3a
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-03 16:08:16 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import Footer from "@/components/footer/footer";
 import HeroSection from "@/components/hero/HeroSection";
-import Nav from "@/components/nav/Nav";
 import Products from "@/components/products/Products";
 import Promotions from "@/components/promotions/Promotions";
 
 export default function Home() {
   return (
     <>
-      <Nav />
       <HeroSection />
       <Promotions />
       <Products />
-      <Footer />
     </>
   );
 }
