ed71c6a8dbc101024b070f6c28da570cca38749e
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-11 23:19:02 +0500

    Create cartItemsSlice.ts

---
+++ src/store/slices/cartItemsSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+// import { cookies } from "next/headers";
+
+type CartProduct = {
+  user_id: string;
+  product_id: string;
+  quantity: number;
+};
+
+// const uid = cookies().get("user_id")?.value;
+
+// if (uid) {
+//   // fetch()
+// }
+
+const initialState = async () => {
+  const res = await fetch("/api/cart", {
+    method: "GET",
+  });
+  const result = await res.json();
+  return result.res;
+  // setItemsFromDb(result.res);
+  // console.log("products from get request", result.res);
+};
+console.log("initial state", initialState);
+
+const CartItemsSlics = createSlice({
+  name: "CartItemsSlice",
+  initialState,
+  reducers: {
+    addItemToCart: (state) => {
+      return state;
+    },
+  },
+});
+
+export default CartItemsSlics.reducer;
