45b9d36e0be0ac9055844f982f1a04127323cd91
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-11 09:33:00 +0500

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import React from "react";
-import {
-  AiOutlineDelete,
-  AiOutlineMinusCircle,
-  AiOutlinePlusCircle,
-} from "react-icons/ai";
+import CartItems from "./CartItems";
 
 export default function page() {
   return (
@@ -14,43 +9,7 @@ export default function page() {
         {/* Cart Box */}
         <div className="flex flex-col gap-4 lg:flex-row ">
           {/* product image and summary container */}
-          <div className="flex flex-col gap-4 md:flex-row  lg:w-2/3 lg:gap-8">
-            {/* image */}
-            <div>
-              <Image
-                src={"/Featured1.webp"}
-                height={300}
-                width={250}
-                alt="product"
-                className="rounded-md"
-              />
-            </div>
-            {/* product details */}
-            <div className="flex flex-grow flex-col md:gap-4">
-              <div className="flex justify-between pt-8 md:pt-0">
-                <div className="text-2xl font-light">
-                  Brushed Raglan Sweatshirt
-                </div>
-                {/* Delete icon */}
-                <AiOutlineDelete size={25} />
-              </div>
-              <div className="py-2 font-semibold text-gray-700">Dress</div>
-              <div className="font-semibold">Delivery Estimateion</div>
-              <div className="py-2 font-semibold text-yellow-500">
-                5 Working days
-              </div>
-              <div className="flex">
-                <div className="text-xl font-semibold">$195</div>
-                <div className="ml-auto flex items-center gap-4">
-                  {/* Minus */}
-                  <AiOutlineMinusCircle size={25} />
-                  <span>1</span>
-                  {/* Plus */}
-                  <AiOutlinePlusCircle size={25} />
-                </div>
-              </div>
-            </div>
-          </div>
+          <CartItems />
           {/* summary */}
           <div className="flex flex-col gap-6 p-8 lg:gap-8 lg:pt-0">
             <h2 className="text-xl font-bold">Order Summary</h2>
