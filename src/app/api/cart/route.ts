0da9b5863afc22c5ca1d743170fb3f3cb1dcc56c
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-11 23:47:00 +0500

    Update route.ts

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { db, cartTable } from "../../../../sanity/lib/drizzle";
 import { v4 as uuid } from "uuid";
 import { cookies } from "next/headers";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 export const GET = async (request: NextRequest) => {
   const uid = cookies().get("user_id")?.value;
@@ -34,33 +34,39 @@ export const GET = async (request: NextRequest) => {
 //     return NextResponse.json({ message: "Something went wrong" });
 //   }
 // };
-
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
   const user_id = cookies().get("user_id")?.value;
-  
+
   if (!user_id) {
     const uid = uuid();
     cookies().set("user_id", uid);
   }
+
   try {
     const existingProduct = await db
-        .select()
-        .from(cartTable)
-        .where(eq(cartTable.product_id, req.product_id));
-    
-    if(!existingProduct){
+      .select()
+      .from(cartTable)
+      .where(
+        and(
+          eq(cartTable.product_id, req.product_id),
+          eq(cartTable.user_id, user_id!)
+        )
+      );
+
+    if (existingProduct.length > 0) {
+      console.log("Product already exists");
+    } else {
       const res = await db
-      .insert(cartTable)
-      .values({
-        product_id: req.product_id,
-        quantity: req.quantity,
-        user_id: cookies().get("user_id")?.value as string,
-      })
-      .returning();
-    return NextResponse.json({ res });
-    }else {
-      console.log('product already exists')
+        .insert(cartTable)
+        .values({
+          product_id: req.product_id,
+          quantity: req.quantity,
+          user_id: cookies().get("user_id")?.value as string,
+        })
+        .returning();
+
+      return NextResponse.json({ res });
     }
   } catch (error) {
     console.log(error);
