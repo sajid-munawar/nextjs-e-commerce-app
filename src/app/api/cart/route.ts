346bbdecb421e433897c9dfc75ca9d32403fafa4
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-11 15:20:10 +0500

    Update route.ts
    Updating api logic for existing products

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -6,9 +6,6 @@ import { eq } from "drizzle-orm";
 
 export const GET = async (request: NextRequest) => {
   const uid = cookies().get("user_id")?.value;
-  // const req = request.nextUrl;
-  // const uid = req.searchParams.get("user_id");
-  // console.log(uid);
   if (uid) {
     try {
       const res = await db
@@ -40,15 +37,20 @@ export const GET = async (request: NextRequest) => {
 
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
-  const uid = uuid();
   const user_id = cookies().get("user_id")?.value;
-
+  
   if (!user_id) {
+    const uid = uuid();
     cookies().set("user_id", uid);
   }
-
   try {
-    const res = await db
+    const existingProduct = await db
+        .select()
+        .from(cartTable)
+        .where(eq(cartTable.product_id, req.product_id));
+    
+    if(!existingProduct){
+      const res = await db
       .insert(cartTable)
       .values({
         product_id: req.product_id,
@@ -57,6 +59,9 @@ export const POST = async (request: NextRequest) => {
       })
       .returning();
     return NextResponse.json({ res });
+    }else {
+      console.log('product already exists')
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "Something went wrong" });
