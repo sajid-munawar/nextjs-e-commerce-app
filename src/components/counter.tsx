ba156bb5c7220f7370aec7795b06d95a38f2f5a7
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-11 13:58:53 +0500

    Update counter.tsx

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -5,26 +5,26 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "@/store/slices/counterSlice";
 
 export function Counter() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  // const count = useSelector((state: RootState) => state.counter.value);
+  // const dispatch = useDispatch();
 
   return (
-    <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
+    // <div>
+    //   <div>
+    //     <button
+    //       aria-label="Increment value"
+    //       onClick={() => dispatch(increment())}
+    //     >
+    //       Increment
+    //     </button>
+    //     <span>{count}</span>
+    //     <button
+    //       aria-label="Decrement value"
+    //       onClick={() => dispatch(decrement())}
+    //     >
+    //       Decrement
+    //     </button>
+    //   </div>
+    // </div>
   );
 }
