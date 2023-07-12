d781979a1072dd8dc21538049a444f21d1829cfd
sajid-munawawr <iamsajiddev@gmail.com>
2023-07-12 16:36:20 +0500

    Update route.ts

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -85,15 +85,16 @@ export const POST = async (request: NextRequest) => {
 
 export const DELETE = async (request: NextRequest) => {
   const req = await request.json();
+  console.log('reqqwert',req)
   const user_id = cookies().get("user_id")?.value;
-  try {
-    const res = await db
-      .delete(cartTable)
-      .where(
-        and(eq(cartTable.product_id, req._id), eq(cartTable.user_id, user_id!))
-      );
-    // return NextResponse.json( res );
-  } catch (error) {
-    return NextResponse.json({ message: "Something went wrong" });
-  }
+  // try {
+  //   const res = await db
+  //     .delete(cartTable)
+  //     .where(
+  //       and(eq(cartTable.product_id, req.id), eq(cartTable.user_id, user_id!))
+  //     );
+  //   // return NextResponse.json( res );
+  // } catch (error) {
+  //   return NextResponse.json({ message: "Something went wrong" });
+  // }
 };
