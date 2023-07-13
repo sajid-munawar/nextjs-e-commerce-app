45a911610a8c94aeda210b1bc7caa3beac31b446
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-13 12:24:07 +0500

    Update cartItemsSlice.ts

--- src/store/slices/cartItemsSlice.ts
+++ src/store/slices/cartItemsSlice.ts
@@ -2,10 +2,7 @@ import { ICartProduct } from "@/interface/interface";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-
-
-const initialState:ICartProduct[] = [];
-
+const initialState: ICartProduct[] = [];
 
 const cartItemsSlice = createSlice({
   name: "cartItems",
@@ -14,7 +11,8 @@ const cartItemsSlice = createSlice({
     addItemToCart: (state, action) => {
       const newItem = action.payload;
       const existingItem = state.find(
-        (item) => item.product_id === newItem.product_id
+        (item) =>
+          item.product_id === newItem.product_id && item.size === newItem.size
       );
       if (!existingItem) {
         return [...state, newItem];
@@ -22,13 +20,14 @@ const cartItemsSlice = createSlice({
       return state;
     },
     removeItemFromCart: (state, action) => {
-      const productId = action.payload;
-      return state.filter((item) => item.product_id !== productId);
+      const { productId, size } = action.payload;
+      return state.filter(
+        (item) => item.product_id !== productId && item.size !== size
+      );
     },
   },
 });
 
 export const { addItemToCart, removeItemFromCart } = cartItemsSlice.actions;
-
 
 export default cartItemsSlice.reducer;
