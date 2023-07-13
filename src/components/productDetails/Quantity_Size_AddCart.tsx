bfa511dddddcba1a5f3dc0b4b4b452081c6da581
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-13 09:43:17 +0500

    Update Quantity_Size_AddCart.tsx

--- src/components/productDetails/Quantity_Size_AddCart.tsx
+++ src/components/productDetails/Quantity_Size_AddCart.tsx
@@ -1,29 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { IProductsDetail } from "../../interface/interface";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useDispatch, useSelector } from "react-redux";
-// import { RootState } from "@/app/store/store";
-import { RootState } from "@/store/store";
-// import type { RootState } from "@/store/store";
-// import { decrement, increment } from "@/store/slices/counterSlice";
+import { useDispatch } from "react-redux";
 import { addItemToCart } from "@/store/slices/cartItemsSlice";
 import { BsCart } from "react-icons/bs";
 import { BiLoaderCircle } from "react-icons/bi";
-import { v4 as uuid } from "uuid";
-
-// import { counterActions } from "@/app/store/slice/CartSlice";
 
 export default function Quantity_Size_AddCart({
   product,
 }: {
   product: IProductsDetail;
 }) {
-  // const dispatch = useDispatch();
-  // const itemCount = useSelector((state: RootState) => state.itemCount.value);
   const [itemsQuanity, setItemsQuantity] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(false);
+  const [selectedSize, setSelectedSize] = useState("m");
   const dispatch = useDispatch();
 
   const handleAddToCart = async () => {
@@ -35,23 +27,10 @@ export default function Quantity_Size_AddCart({
       price: product.price,
       title: product.title,
       category: product.category,
+      size: selectedSize,
     };
     toast.success("Item added to cart");
-
     dispatch(addItemToCart(newItem));
-    // const res = await fetch("/api/cart", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     product_id: product._id,
-    //     quantity: itemsQuanity,
-    //   }),
-    // });
-    // if (res.ok) {
-    //   toast.success("Item added to cart");
-    // }
-    setLoading(false);
-    // const result = await res.json();
-    // console.log(result);
   };
   const handleInrease = () => {
     setItemsQuantity(itemsQuanity + 1);
@@ -61,6 +40,10 @@ export default function Quantity_Size_AddCart({
     if (itemsQuanity > 1) {
       setItemsQuantity(itemsQuanity - 1);
     }
+  };
+
+  const handleSizeSelection = (size: string) => {
+    setSelectedSize(size);
   };
 
   return (
@@ -80,47 +63,47 @@ export default function Quantity_Size_AddCart({
           <div className="text-sm font-bold leading-4 tracking-wider text-textBlack">
             SELECT SIZE
           </div>
-          {/* Test div for total items in redux */}
-          {/* <div>
-            <ul>
-              {allitems.map((i) => (
-                <li
-                  key={`${i.id}${i.size}`}
-                >{`id:${i.id} quantity${i.quantity} size${i.size} price${i.totalPrice}`}</li>
-              ))}
-            </ul>
-          </div> */}
-          <div className="flex gap-3 ">
-            <button
-            // onClick={() => handleSize("XS")}
-            // className={getButtonClassName("XS")}
+          <div className="flex flex-wrap gap-4">
+            <div
+              className={`flex h-8 w-8 cursor-pointer items-center justify-center rounded-full ${
+                selectedSize === "xs" ? "bg-black/70 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => handleSizeSelection("xs")}
             >
               XS
-            </button>
-            <button
-            // onClick={() => handleSize("S")}
-            // className={getButtonClassName("S")}
+            </div>
+            <div
+              className={`flex h-8 w-8 cursor-pointer items-center justify-center rounded-full ${
+                selectedSize === "s" ? "bg-black/70 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => handleSizeSelection("s")}
             >
               S
-            </button>
-            <button
-            // onClick={() => handleSize("M")}
-            // className={getButtonClassName("M")}
+            </div>
+            <div
+              className={`flex h-8 w-8 cursor-pointer items-center justify-center rounded-full ${
+                selectedSize === "m" ? "bg-black/70 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => handleSizeSelection("m")}
             >
               M
-            </button>
-            <button
-            // onClick={() => handleSize("L")}
-            // className={getButtonClassName("L")}
+            </div>
+            <div
+              className={`flex h-8 w-8 cursor-pointer items-center justify-center rounded-full ${
+                selectedSize === "l" ? "bg-black/70 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => handleSizeSelection("l")}
             >
               L
-            </button>
-            <button
-            // onClick={() => handleSize("XL")}
-            // className={getButtonClassName("XL")}
+            </div>
+            <div
+              className={`flex h-8 w-8 cursor-pointer items-center justify-center rounded-full ${
+                selectedSize === "xl" ? "bg-black/70 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => handleSizeSelection("xl")}
             >
               XL
-            </button>
+            </div>
           </div>
         </div>
         <div className="flex items-center gap-8 ">
@@ -128,17 +111,11 @@ export default function Quantity_Size_AddCart({
             Quantity
           </div>
           <div className="flex items-center gap-1">
-            <button
-              //  onClick={smallDec}
-              onClick={handleDescrease}
-              className="mr-2 cursor-pointer"
-            >
+            <button onClick={handleDescrease} className="mr-2 cursor-pointer">
               -
             </button>
-            {/* <span>{specificItem?.quantity}</span> */}
             <span>{itemsQuanity}</span>
             <button
-              // onClick={smallInc}
               onClick={handleInrease}
               className="ml-2 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border-2 border-textBlack"
             >
@@ -151,7 +128,6 @@ export default function Quantity_Size_AddCart({
             {!loading ? (
               <button
                 onClick={handleAddToCart}
-                // onClick={addCartIncrement}
                 className="flex flex-row items-center justify-center gap-3"
               >
                 <BsCart size={20} />
@@ -170,70 +146,3 @@ export default function Quantity_Size_AddCart({
     </>
   );
 }
-
-// const user_size = useSelector((state: RootState) => state.CartSlice.size);
-// const value = useSelector((state: RootState) => state.CartSlice.value);
-// const allitems = useSelector((state: RootState) => state.CartSlice.items);
-
-// post item on DB
-// const postCartItemtoDB = async () => {
-//   toast.loading(`Wait... Adding to cart`);
-//   const res = await fetch("/api/cart", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       product_id: product.title,
-//       size: user_size,
-//       quantity: value,
-//       price: product.price,
-//     }),
-//   });
-// const result = await res.json();
-// console.log(result);
-//   toast.dismiss();
-// };
-
-// // Update selected size state
-// const handleSize = (curSize: string) => {
-//   dispatch(counterActions.updateSize(curSize));
-// };
-// // Selected Size CSS
-// const getButtonClassName = (buttonSize: string) => {
-//   return `min-w-[23px] cursor-pointer rounded-full p-[1px] text-center text-base font-bold text-textGrey hover:shadow-lg hover:shadow-gray-400 ${
-//     user_size === buttonSize ? "shadow-xl shadow-gray-500" : ""
-//   }`;
-// };
-
-// // Add items to cart
-// const addCartIncrement = async () => {
-//   if (value > 0 && user_size !== "") {
-//     dispatch(
-//       counterActions.addToCart({
-//         product: {
-//           id: product.title,
-//           price: parseInt(product.price, 10),
-//           size: user_size,
-//         },
-//         quantity: value,
-//       })
-//     );
-//     await postCartItemtoDB();
-//     toast(`${product.title} added to cart`);
-//   } else {
-//     if (user_size === "") {
-//       toast.error(`Please select size`);
-//     } else {
-//       toast.error(`Please Select quantity`);
-//     }
-//   }
-// };
-
-// const smallInc = () => {
-//   if (value < 5) {
-//     dispatch(counterActions.smallIncrement(1));
-//   }
-// };
-// const smallDec = () => {
-//   if (value > 0) {
-//     dispatch(counterActions.smallDecrement(1));
-//   }
-// };
