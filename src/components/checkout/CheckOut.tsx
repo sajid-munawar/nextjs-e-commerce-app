2a9aa4a4c018941a7c56de37b7d1d8559b31130d
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-17 07:43:12 +0500

    Update CheckOut.tsx

--- src/components/checkout/CheckOut.tsx
+++ src/components/checkout/CheckOut.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import getStripePromise from "@/lib/stripe";
 
 const CheckOut = () => {
   const product = [
     {
       price: 10,
       name: "Abc",
+      quantity: 1,
     },
   ];
   const handleCheckOut = async () => {
+    const stripe = await getStripePromise();
     const res = await fetch("/api/stripe-session", {
       method: "POST",
       body: JSON.stringify(product),
     });
+    const data = await res.json();
+    if (data.session) {
+      stripe?.redirectToCheckout({ sessionId: data.session.id });
+    }
   };
   return (
     <>
