41a066a3b026cbffbeb35f3a75894ed90b85e6ce
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-17 07:43:06 +0500

    Update route.ts

--- src/app/api/stripe-session/route.ts
+++ src/app/api/stripe-session/route.ts
@@ -9,5 +9,49 @@ const stripe = new Stripe(key, {
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
-  console.log("body :>> ", body);
+  try {
+    if (body.length > 0) {
+      const session = await stripe.checkout.sessions.create({
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1NUgwsAX0f3qFERb5VC769A8" },
+          { shipping_rate: "shr_1NUgxjAX0f3qFERbBRGxrPaC" },
+        ],
+        invoice_creation: {
+          enabled: true,
+        },
+        line_items: body.map((item: any) => {
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+              maximum: 10,
+            },
+          };
+        }),
+        phone_number_collection: {
+          enabled: true,
+        },
+        success_url: `${request.headers.get("origin")}/success`,
+        cancel_url: `${request.headers.get("origin")}/?canceled=true`,
+      });
+      return NextResponse.json({ session });
+    } else {
+      return NextResponse.json({ message: "No Data Found" });
+    }
+  } catch (err: any) {
+    console.log(err);
+    return NextResponse.json(err.message);
+  }
 };
