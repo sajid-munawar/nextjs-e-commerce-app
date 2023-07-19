7b0d40c49001df71dc7e7900efe6971e1d4c7728
Sajid Munawar <iamsajiddev@gmail.com>
2023-07-19 10:38:05 +0500

    Locally working fine

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,8 @@
 import Stripe from "stripe";
+import { NextRequest, NextResponse } from "next/server";
+import { db, ordersTable } from "../../../../sanity/lib/drizzle";
+
+import { and, eq } from "drizzle-orm";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2022-11-15",
@@ -13,8 +17,6 @@ export async function POST(req: any, res: any) {
     if ("checkout.session.completed" === event.type) {
       const session: any = event.data.object;
       console.log("session :>> ", session);
-      //Once you'll get data you can use it according to your
-      //reqirement for making update in DB
       const {
         id,
         amount_subtotal,
@@ -23,16 +25,39 @@ export async function POST(req: any, res: any) {
         currency,
         customer,
         status,
-        customer_details: { name, email, phone },
-        shipping_cost: { amount_total: shipping_amount_total },
+        customer_details,
+        shipping_cost,
       } = session;
+      const { name, email, phone } = customer_details;
+      const { amount_total: shipping_amount_total } = shipping_cost;
+
+      const res = await db
+        .insert(ordersTable)
+        .values({
+          id,
+          amount_subtotal,
+          amount_total,
+          created,
+          currency,
+          customer,
+          status,
+          customer_name: name,
+          customer_email: email,
+          customer_phone: phone,
+          shipping_amount_total,
+        })
+        .returning();
+
+      console.log("data inserted into table", res);
+
+      return NextResponse.json({ res });
     } else {
       res.setHeader("Allow", "POST");
-      // res.status(405).end("Method Not Allowed");
+      res.status(405).end("Method Not Allowed");
     }
   } catch (err: any) {
     console.log("Error in webhook----------", err);
-    // res.status(400).send(`Webhook Error: ${err.message}`);
+    res.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
 }
